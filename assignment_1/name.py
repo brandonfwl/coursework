e16e31a7581daf60f404063389f0b59e2da619a8
brandonfwl <brandonfwl@gmail.com>
2017-02-05 18:37:44 -0800

    for redundancy i added the name.py just to be sure

---
+++ assignment_1/name.py
@@ -0,0 +1,15 @@
+
+"""
+
+    full_name.py
+    Brandon Fowler
+    CS 131A A. Brick
+
+"""
+
+
+def full_name():
+    # function to print my full name.
+    print('--------------------\n\n Brandon Fowler\n\n -------------------')
+
+full_name()
