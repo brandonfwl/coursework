5211b4fe17059ebd662fe87a2ef34d6fb61d04c0
brandonfwl <brandonfwl@gmail.com>
2017-02-26 17:35:38 -0800

    completed assignment 2

--- assignment_2/reverse.py
+++ assignment_2/reverse.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3.5
+'''
+Assignment 2
+Brandon Fowler
+CS131A
+02-26-2017
+
+'''
 
 import sys
 
-print('Command Line Arguments in reverse order', str(sys.argv[::-1]))
+r_stage = sys.argv[::-1]
+
+for i in range(len(r_stage)):
+    if 'reverse.py' in r_stage[i]:
+        pass
+    else:
+        print('Argument', i + 1, ' ', r_stage[i])
